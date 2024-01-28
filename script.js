8279e71dc8aae543207daea90c51c7ec3b169c23
maheshvpawar001 <maheshvpawar001@gmail.com>
2024-01-28 14:24:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function addToCart(productName, price) {
+    alert('Added to cart: ' + productName + ' - $' + price);
+    // You can add more logic here, such as updating a shopping cart, navigating to a checkout page, etc.
+}
